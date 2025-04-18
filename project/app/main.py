7f00387ac8582d9f48d2ef659d40bf8b59767abd
Ian4634 <flyingiancheng@gmail.com>
2025-04-18 11:57:31 +0800

    added email and address

--- project/app/main.py
+++ project/app/main.py
@@ -1,8 +1,5 @@
 print("Time Capsule")
 print("Registration Start")
 print("Enter Name")
-<<<<<<< HEAD
 print("Enter Address")
-=======
 print("Enter Email")
->>>>>>> 9101962391856a982dd588947dfea6db5ff77374
